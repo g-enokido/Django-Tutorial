f07606cbeadc30abefc4d771c6af17e8e31289db
Hayato Enokido <hayato_enokido@galaxy.jp>
2019-09-30 10:45:54 +0900

    My Django Girls app, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\glxy442\documents\django\product_no1\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
