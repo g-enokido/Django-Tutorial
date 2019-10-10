eeec3ffe91eca4b7f56302902a86240f1e7eb338
g-enokido <hayato_enokido@galaxy.jp>
2019-10-10 11:44:38 +0900

    ログイン画面を独自の定義のものに変更

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from .models import CustomUser
 from .models import Blog
 
 
@@ -20,3 +20,10 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['blog_name', 'blog_detail', ]
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
