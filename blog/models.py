3b2e6b99b665474f71d14e842966433d914fb639
g-enokido <hayato_enokido@galaxy.jp>
2019-10-16 14:41:59 +0900

    ブログ・カテゴリ・記事を関連付けるように変更

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,17 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from accounts.models import Blog
+
+
+class Category(models.Model):
+    holder = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=200, verbose_name='カテゴリー名')
+    blog = models.ForeignKey(Blog, verbose_name='掲載ブログ',
+                             on_delete=models.CASCADE, default=0)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField()
 
 
 class Post(models.Model):
@@ -9,11 +20,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name='タイトル')
     text = models.TextField(verbose_name='投稿内容')
     created_date = models.DateTimeField(default=timezone.now)
-    blog_id = models.IntegerField()
-    category_id = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, verbose_name='掲載ブログ',
+                                 on_delete=models.CASCADE, default=0)
     image = models.ImageField(
         upload_to='pictures/', null=True, blank=True)
     published_date = models.DateTimeField(blank=True, null=True)
+    blog = models.ForeignKey(Blog, verbose_name='掲載ブログ',
+                             on_delete=models.CASCADE, default=0)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -21,12 +34,3 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.title)
-
-
-class Category(models.Model):
-    holder = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    category_name = models.CharField(max_length=200, verbose_name='カテゴリー名')
-    blog_id = models.IntegerField()
-    created_date = models.DateTimeField(default=timezone.now)
-    updated_date = models.DateTimeField()
--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,8 @@ from .forms import PostForm, CategoryForm
 
 def index(request):
     posts = Blog.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date').reverse()[:5]
+        published_date__lte=timezone.now()).order_by('published_date').reverse().select_related(
+        "Post").count() > 0[:5]
 
     if 'blog' in request.session:
         del request.session['blog']
