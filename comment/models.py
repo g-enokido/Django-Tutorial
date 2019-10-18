fc5e562b289aced89edd447a67e486c85bf79dcf
g-enokido <hayato_enokido@galaxy.jp>
2019-10-18 10:25:49 +0900

    コメント機能の元ブログ・元記事に外部キー制約を追加

--- comment/models.py
+++ comment/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.utils import timezone
+from blog.models import Post
+from accounts.models import Blog
+
 
 # Create your models here.
 
 
 class Comment(models.Model):
-    author = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, default='名無しさん')
     text = models.TextField()
-    article_id = models.IntegerField()
-    blog_id = models.IntegerField(default=0)
+    article = models.ForeignKey(Post, verbose_name='元記事',
+                                on_delete=models.CASCADE, default=0)
+    blog = models.ForeignKey(Blog, verbose_name='元ブログ',
+                             on_delete=models.CASCADE, default=0)
     created_date = models.DateField(default=timezone.now)
 
     def publish(self):
--- comment/views.py
+++ comment/views.py
@@ -1,6 +1,8 @@
 from django.views.decorators.csrf import csrf_protect
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from .forms import CommentForm
+from blog.models import Post
+from accounts.models import Blog
 
 
 @csrf_protect
@@ -11,12 +13,10 @@ def Comment(request, pk):
             post = form.save(commit=False)
             if request.user.is_authenticated:
                 post.author = request.user
-            elif request.POST["author"] is None:
-                post.author = '名無しさん'
             else:
                 post.author = form["author"].value()
-            post.article_id = pk
-            post.blog_id = request.session['blog'].id
+            post.article = get_object_or_404(Post, id=pk)
+            post.blog = get_object_or_404(Blog, id=post.article.blog_id)
             post.save()
             return redirect('post_detail', pk=pk)
         return redirect('post_detail', pk=pk)
