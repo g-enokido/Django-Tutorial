888861923598db0785317db9096fc064ee0cd084
g-enokido <hayato_enokido@galaxy.jp>
2019-11-08 14:52:56 +0900

    画像を複数投稿し、表示ができるように修正。

--- blog/static/javascripts/to_post_edit.js
+++ blog/static/javascripts/to_post_edit.js
@@ -1,9 +1,28 @@
-function add_images(name, pk) {
-    var select = document.getElementById('id_category');
-    var option = document.cleateElement('option');
-    option.setAttribute('value', pk);
-    option.innerHTML = name;
+function add_images(name) {
+    var option = window.opener.document.getElementById('id_text')
+    var nowArray = option.value.split("\n");
+    var imageset = "";
+    if (nowArray.length == 0) {
+        imageset = "<img src=" + name + " /> <br>";
+    } else {
+        imageset = "<br><img src=" + name + " /><br>";
+    }
 
-    select.addEventListener(option, 0)
-    select.options[0].selected = true
+    nowArray.push(imageset)
+    var resArray = [];
+    for (var i = 0; i < nowArray.length; i++) {
+        if (nowArray[i].length > 0) {
+            resArray.push(nowArray[i]);
+        }
+    }
+
+    var retString = "";
+    for (var i = 0; i < resArray.length; i++) {
+        retString += resArray[i] + "\n";
+    }
+    option.value = retString;
+}
+
+function PopupOpener(name) {
+    window.open(name, 'subwin', 'width=500,height=500')
 }
